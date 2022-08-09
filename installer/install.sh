698fc94fef9ba1e3e837783716f69df57ba3a875
Daniel Molinero Lucas <danielmolinero111@gmail.com>
2022-08-09 18:15:11 +0200

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -60,4 +60,4 @@ rm -rf $TEMP
 #Display installation path
 echo "\nZSH-Ultra Path: ${ZSH-ULTRA}"
 #Set PATH
-echo $PATH | grep -q "\(^\|:\)$DOTNET_ROOT\(:\|/\{0,1\}$\)" || echo "PATH=\$PATH:$DOTNET_ROOT" >> ~/.zshrc; source ~/.zshrc
+echo $PATH | grep -q "\(^\|:\)$DOTNET_ROOT\(:\|/\{0,1\}$\)" || echo "PATH=\$PATH:$DOTNET_ROOT" >> ~/.zshrc; . ~/.zshrc
